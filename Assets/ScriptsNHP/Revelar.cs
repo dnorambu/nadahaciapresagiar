2878bc5325dfc20fd7e2b0d6fc6fe5d2f574fe99
Diego Norambuena <diego.norambuena@sansano.usm.cl>
2021-11-27 19:52:28 -0300

    Se arregla el script de revelar
    Se añade la quest del NPC

--- Assets/ScriptsNHP/Revelar.cs
+++ Assets/ScriptsNHP/Revelar.cs
@@ -8,6 +8,7 @@ public class Revelar : MonoBehaviour
 {
     private bool fadeOut, fadeIn;
     public Transform player;
+    public SpriteRenderer OtraParte = null;
     public float fadeSpeed = 0.8f;
     float distance;
     public void fadeOutObject()
@@ -28,7 +29,8 @@ public class Revelar : MonoBehaviour
         if (Input.GetKey(KeyCode.Q))
         {
             distance = Vector3.Distance(player.position, this.transform.position);
-            print(distance);
+            //print(distance);
+            //print(OtraParte.color.r);
             if (distance <= 1f){
                 fadeOutObject();
             }
@@ -38,9 +40,15 @@ public class Revelar : MonoBehaviour
         {
             Color objectColor = this.GetComponent<SpriteRenderer>().color;
             float fadeAmount = objectColor.a - (fadeSpeed * Time.deltaTime);
-
             objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
             this.GetComponent<SpriteRenderer>().color = objectColor;
+            if (OtraParte != null)
+            {
+                Color objectColor2 = OtraParte.color;
+                float fadeAmount2 = objectColor2.a - (fadeSpeed * Time.deltaTime);
+                objectColor2 = new Color(objectColor2.r, objectColor2.g, objectColor2.b, fadeAmount2);
+                OtraParte.color = objectColor2;
+            }
             if (objectColor.a < 0.3 || !Input.GetKey(KeyCode.Q) )
             {
                 fadeInObject();
@@ -53,6 +61,15 @@ public class Revelar : MonoBehaviour
 
             objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
             this.GetComponent<SpriteRenderer>().color = objectColor;
+
+            if( OtraParte != null)
+            {
+                Color objectColor2 = OtraParte.color;
+                float fadeAmount2 = objectColor2.a + (fadeSpeed * Time.deltaTime);
+
+                objectColor2 = new Color(objectColor2.r, objectColor2.g, objectColor2.b, fadeAmount2);
+                OtraParte.color = objectColor2;
+            }
 
             if (objectColor.a > 1)
             {
