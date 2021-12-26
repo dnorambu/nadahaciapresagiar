534e9542e6072f55bf66c2651be3e5e9d2386c0b
Diego Norambuena <diego.norambuena@sansano.usm.cl>
2021-12-26 04:13:36 -0300

    Cambios finales
    La escena 3 está finita

---
+++ Assets/ScriptsNHP/CambiarAnimacion.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPGM.Gameplay;
+
+public class CambiarAnimacion : MonoBehaviour
+{
+    public Quest questFinal = null;
+
+    public AnimatorOverrideController animNuevo = null;
+    bool isChanged = false;
+    Animator anim; 
+    // Update is called once per frame
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if(questFinal != null && !isChanged)
+        {
+            if (questFinal.isStarted)
+            {
+
+                Debug.Log("Si esta entrando aqui");
+                isChanged = true;
+                anim.runtimeAnimatorController = animNuevo;
+            }
+        }
+    }
+}
